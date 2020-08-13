feaf5c5b7a1830dddf3410a104bc266ee9a80e5f
andrenevares <andre.nevares@gmail.com>
2020-08-13 16:48:20 -0300

    parte 03

--- projeto/polls/urls.py
+++ projeto/polls/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
 from . import views
 
+# Neste tutorial temos um app apenas.  Mas em um projeto podemos ter vários apps com seus urls.py
+# Imaginemos que tempos dois apps.  Um polls e outro products... Por exemplo
+# Se ambos tiverem url com name='detail'
+# Como o Django vai saber a que app você está se referindo?  
+# para isso vamos colocar o app_name
+# quando usarmos o {% url 'detail' %} passaremos a usar {% url 'polls:detail' %}
+
+app_name = 'polls'
+
 urlpatterns = [
+    # Exemplo: /polls/
     path('', views.index, name='index'),
-    
+    # Exemplo: /polls/1/
+    path('<int:question_id>', views.detail, name='detail'),
+    # Exemplo: /polls/1/results
+    path('<int:question_id>/results/', views.results, name='results'),
+    # Exemplo: /polls/1/vote
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- projeto/polls/views.py
+++ projeto/polls/views.py
@@ -1,5 +1,29 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 from django.http import HttpResponse
+from django.http import Http404
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    lista_das_ultimas_questoes = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([questao.question_text for questao in lista_das_ultimas_questoes])
+    context = {
+        'lista_das_ultimas_questoes': lista_das_ultimas_questoes,
+    }
+
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = get_object_or_404(Question, pk=question_id)
+    except:
+        raise Http404(f"A questão nº {question_id} aparentemente não existe")
+
+    return render (request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Você está votando na questão de nº %s." % question_id)
